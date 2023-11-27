ae0fe389b6ac8440c2915e8a3bd5dbfcc3d23b79
user122904 <127532980+user122904@users.noreply.github.com>
2023-11-27 23:47:23 +0800

    Add project files.

---
+++ Mysql/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Mysql.iptclass;
+
+namespace Mysql
+{
+    public partial class Form1 : Form
+    {
+        mysqlconnect con = new mysqlconnect();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.connectdb.Open();
+                label1.Text = "You are Connected to MySql Database!";
+                label1.ForeColor = Color.Green;
+                con.connectdb.Close();
+            }
+            catch (Exception)
+            {
+
+                label1.Text = "Failed to Connect to MySql Database!";
+                label1.ForeColor = Color.Red;
+            }
+        }
+    }
+}
---
+++ Mysql/iptclass/mysqlconnect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Mysql.iptclass
+{
+    internal class mysqlconnect
+    {
+        public MySqlConnection connectdb;
+
+        public mysqlconnect()
+        {
+            string host = "localhost";
+            string database = "iptstudent";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+
+            string connection_string = "datasource =" + host + "; database =" + database + "; port =" + port + "; username =" + username + ";" + "password ="
+                + password + "; SslMode = none;";
+
+            connectdb = new MySqlConnection(connection_string);
+
+        }
+
+    }
+}
